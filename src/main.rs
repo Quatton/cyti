b6c5668703c9fddfd1d11e87f257c8bc3ef68cc0
Quatton <atomicatives@gmail.com>
2024-05-19 20:00:59 +0900

    car game

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use bevy::{
-    gltf::{Gltf, GltfMesh, GltfNode},
-    prelude::*,
-};
+use bevy::{gltf::Gltf, prelude::*};
 use bevy_asset_loader::prelude::*;
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
 use bevy_rapier3d::prelude::*;
@@ -19,6 +16,25 @@ struct MyAssets {
     pub taxi: Handle<Gltf>,
 }
 
+#[derive(Component)]
+struct Following {
+    facing: Vec3,
+}
+
+#[derive(Component)]
+struct CanDie;
+
+#[derive(Component)]
+struct Player;
+
+impl Default for Following {
+    fn default() -> Self {
+        Self {
+            facing: Vec3::new(0.0, 0.0, 1.0),
+        }
+    }
+}
+
 fn main() {
     App::new()
         .init_state::<AssetLoaderState>()
@@ -33,6 +49,10 @@ fn main() {
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugins(RapierDebugRenderPlugin::default())
         .add_plugins(PanOrbitCameraPlugin)
+        .add_systems(Update, (rotate_car, move_car))
+        .add_systems(Update, spawn_car_on_c)
+        .add_systems(Update, kill_out_of_bounds)
+        .add_systems(Update, respawn_player_if_not_exists)
         .run();
 }
 
@@ -54,33 +74,6 @@ fn setup(
         Collider::cylinder(0.1, 30.0),
     ));
 
-    commands
-        .spawn((
-            SceneBundle {
-                scene: assets.load("cars/taxi.glb#Scene0"),
-                transform: Transform::from_xyz(0.0, 10.0, 0.0),
-                ..default()
-            },
-            RigidBody::Dynamic,
-        ))
-        .with_children(|p| {
-            p.spawn((
-                Collider::cuboid(0.5, 0.5, 1.0),
-                Transform::from_xyz(0.0, 0.5, 0.0),
-                Restitution::coefficient(0.5),
-            ));
-
-            p.spawn(PointLightBundle {
-                point_light: PointLight {
-                    shadows_enabled: true,
-                    radius: 10.0,
-                    intensity: 100000.0,
-                    ..default()
-                },
-                transform: Transform::from_xyz(0.0, 0.5, 2.0),
-                ..default()
-            });
-        });
     commands.spawn((
         Camera3dBundle {
             transform: Transform::from_xyz(0.0, 15.0, 45.0).looking_at(Vec3::ZERO, Vec3::Y),
@@ -88,4 +81,120 @@ fn setup(
         },
         PanOrbitCamera::default(),
     ));
+}
+
+fn rotate_car(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&CanDie, &mut Transform)>,
+) {
+    for (_, mut transform) in query.iter_mut() {
+        if keyboard_input.pressed(KeyCode::ArrowRight) {
+            // oneway.facing = Quat::from_rotation_y(-0.1) * oneway.facing;
+            transform.rotation = Quat::from_rotation_y(-0.1) * transform.rotation;
+        }
+
+        if keyboard_input.pressed(KeyCode::ArrowLeft) {
+            // oneway.facing = Quat::from_rotation_y(0.1) * oneway.facing;
+            transform.rotation = Quat::from_rotation_y(0.1) * transform.rotation;
+        }
+
+        let cur = transform.rotation;
+
+        if keyboard_input.pressed(KeyCode::ArrowUp) {
+            // oneway.facing = Quat::from_rotation_y(0.1) * oneway.facing;
+            transform.translation += cur * Vec3::new(0.0, 0.0, 0.5);
+        }
+
+        if keyboard_input.pressed(KeyCode::Space) {
+            // oneway.facing = Quat::from_rotation_y(0.1) * oneway.facing;
+            transform.translation += Vec3::new(0.0, 0.1, 0.0);
+
+            // rotate the x and then remove the y component
+            let relative_x = (cur * Vec3::X).reject_from_normalized(Vec3::Y);
+
+            // rotate the car on the relative x axis
+            transform.rotation = Quat::from_axis_angle(relative_x, -0.1) * transform.rotation;
+        }
+    }
+}
+
+fn spawn_car_on_c(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    commands: Commands,
+    assets: Res<AssetServer>,
+) {
+    if keyboard_input.just_pressed(KeyCode::KeyC) {
+        spawn_car(commands, assets, false);
+    }
+}
+
+fn spawn_car(mut commands: Commands, assets: Res<AssetServer>, as_player: bool) {
+    let mut binding = commands.spawn((
+        SceneBundle {
+            scene: assets.load("cars/taxi.glb#Scene0"),
+            transform: Transform::from_xyz(0.0, 10.0, 0.0),
+            ..default()
+        },
+        RigidBody::Dynamic,
+        CanDie,
+    ));
+
+    let cmd = binding.with_children(|p| {
+        let mut cmd = p.spawn((
+            Collider::cuboid(0.5, 0.5, 1.0),
+            Transform::from_xyz(0.0, 0.5, 0.0),
+            Restitution::coefficient(0.5),
+        ));
+
+        if as_player {
+            cmd.insert(ColliderMassProperties::Density(20.0));
+        } else {
+            cmd.insert(ColliderMassProperties::Density(0.5));
+        }
+
+        p.spawn(PointLightBundle {
+            point_light: PointLight {
+                shadows_enabled: true,
+                radius: 10.0,
+                intensity: 100000.0,
+                ..default()
+            },
+            transform: Transform::from_xyz(0.0, 0.5, 2.0),
+            ..default()
+        });
+    });
+
+    if as_player {
+        cmd.insert(Player);
+    } else {
+        cmd.insert(Following::default());
+    }
+}
+
+fn move_car(mut query: Query<(&Following, &mut Transform)>, time: Res<Time>) {
+    for (_, mut transform) in query.iter_mut() {
+        let rot = transform.rotation;
+        transform.translation += rot * Vec3::new(0.0, 0.0, 5.0) * time.delta_seconds();
+    }
+}
+
+fn kill_out_of_bounds(
+    mut commands: Commands,
+    mut query: Query<(Entity, &Transform), With<CanDie>>,
+) {
+    for (entity, transform) in query.iter_mut() {
+        if transform.translation.y < -10.0 {
+            commands.entity(entity).despawn_recursive();
+        }
+    }
+}
+
+fn respawn_player_if_not_exists(
+    commands: Commands,
+    assets: Res<AssetServer>,
+    query: Query<&Player>,
+) {
+    if query.iter().count() == 0 {
+        spawn_car(commands, assets, true);
+    }
 }
